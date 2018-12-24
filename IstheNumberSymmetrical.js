1eda9ed3e997b5abc3e49f1d165adb4486450cf6
Angel Wu <willjxc@gmail.com>
2018-12-24 13:09:23 -0500

    solved

---
+++ IstheNumberSymmetrical.js
@@ -0,0 +1,31 @@
+Is the Number Symmetrical?
+Create a function that takes a number as an argument and returns true or false depending on whether the number is symmetrical or not. A number is symmetrical when it is the same as its reverse.
+Examples
+isSymmetrical(7227) ➞ true
+
+isSymmetrical(12567) ➞ false
+
+isSymmetrical(44444444) ➞ true
+
+isSymmetrical(9939) ➞ false
+
+isSymmetrical(1112111) ➞ true
+
+
+//============== My solution ===============
+
+
+function isSymmetrical(num) {
+	return Number(num.toString().split('').reverse().join('')) === num
+}
+
+
+//================ Alternative Solutions ===============
+
+
+const isSymmetrical = num => {
+	const str = String(num);
+	return str.split('').reverse().join('') === str;
+}
+ 
+ isSymmetrical=n=>(""+n).split("").reverse().join("")==n;
